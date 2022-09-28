81b2b524bbb250b0100e6195417fabef757cf8d4
Jose <cruzacostajosemanuel@gmail.com>
2022-09-28 18:48:28 +0100

    'ud1'

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title1: 'Vamos', title2: 'Las Palmas'});
 });
 
 module.exports = router;
